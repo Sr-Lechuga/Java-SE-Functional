895d10f2abc2a5b6001de6f187c0de8d389352de
Sr.Lechuga <jonattanlima@gmail.com>
2022-02-04 14:06:42 -0300

    Transforming into a List

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -1,5 +1,7 @@
 package com.platzi.functional._15_streams_intro;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TypeStream {
@@ -7,12 +9,9 @@ public class TypeStream {
         IntStream infiniteStream = IntStream.iterate(0,x -> x + 1);
         /*It generates numbers until infinite.
         * so, it is needed to define when to stop*/
-        infiniteStream.limit(1000) //Stop at 1000
-                .parallel() //divides the work into threads, use concurrency. But it returns values dis-ordered
+        List<Integer> numbersList = infiniteStream.limit(1000) //Stop at 1000
                 .filter(x -> x % 2 == 0) //just take even numbers
-                /*.allMatch()-- it's a predicate that takes an int value and returns a boolean, used to validate a list
-                                like a list of emails
-                 */
-                .forEach(System.out::println);
+                .boxed() //Transforms into a specific type of Stream
+                .collect(Collectors.toList());
     }
 }
